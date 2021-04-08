6a526266ea08b966e125d4224cffc07725ff7dd5
Brycew30 <brycew30@gmail.com>
2021-04-08 09:26:25 -0500

    Done.

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,1 +1,28 @@
-// Code The Spaceship Component Here
+import React from 'react';
+
+class Spaceship extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+
+      <div>
+      <h1>{this.props.name}</h1>
+      <h1>{this.props.speed}</h1>
+      <h1>{this.props.hasRockets}</h1>
+      <h1>{this.props.colors}</h1>
+
+      </div>
+    )
+  }
+}
+
+Spaceship.defaultProps = {
+  speed: 'slow',
+  hasRockets: false,
+  colors: ['black', 'red']
+}
+
+export default Spaceship;
